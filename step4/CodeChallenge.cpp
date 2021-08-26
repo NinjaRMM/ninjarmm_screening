a19ae97a5388d23959cf79b4622fcb4ff1be7df5
kefferphilip <63214533+kefferphilip@users.noreply.github.com>
2021-08-26 08:47:00 -0400

    step four template overload
    step four template overload

--- step4/CodeChallenge.cpp
+++ step4/CodeChallenge.cpp
@@ -50,6 +50,23 @@ T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 =
     return u.getResult(param1, param2, param3);
 }
 
+template <typename R, typename S>
+R checkIfPositive(const S& u, const int param1, const int param2, const int param3, int& ttt)
+{
+
+    // Assume there's already some existing code in here, some business logic that we are not interested in this exercise. 
+    /*
+
+    SOME EXISTING CODE
+
+    */
+
+   ttt = param1 + param2 + param3;
+
+    return u.getResult(param1, param2, param3);
+}
+
+
 //<<<<<<<< END OF EDITABLE SECTION OF CODE <<<<<<<<<<<
 
 int main()
@@ -78,7 +95,7 @@ int main()
     //How are you going to solve this problem? 
     //You can only change the code inside the EDITABLE SECTION OF CODE
     //Hint, the following calls should now be valid moving forward:
-    /*
+
     int total = 0;
 
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2);
@@ -98,7 +115,7 @@ int main()
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total); //<-- Total is output param
     std::cout << sResult << " " << total << std::endl;
-    */
+
 
     return 0;
 
