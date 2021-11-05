e56dc276a3d8bce9dde2201afd83301e963a0315
Francis Joseph Serina <francis.serina@gmail.com>
2021-11-06 00:57:26 +0800

    Implemented RAII
    Removed helper functions
    Added memory leak check in main

--- step3/CodeReview.cpp
+++ step3/CodeReview.cpp
@@ -33,223 +33,235 @@ struct ThirdPartyAVSoftware // move to a header, See src/CodeReview.h
 };
 #endif
 
-// Helper Functions
-bool GetProductList(IWSCProductList** productList);
-bool InitializeProductList(IWSCProductList* productList);
-bool GetProductListCount(IWSCProductList* productList, long& count);
-bool GetProduct(IWSCProductList* productList, long i, IWscProduct** product);
-bool GetProductInfo(IWscProduct* product, ThirdPartyAVSoftware& software);
-bool GetProductName(IWscProduct* product, std::wstring& outName);
-bool GetProductState(IWscProduct* product, std::wstring& outState);
-bool GetDefinitionStatus(IWscProduct* product, std::string& outStatus);
-bool GetDefinitionUpdateTime(IWscProduct* product, std::wstring& outTime);
+class Product
+{
+private:
+    IWscProduct* raw; // RAII
+    bool isGood;
+
+    void GetName(std::wstring& name)
+    {
+        assert(isGood);
+
+        auto PtrVal = BSTR(nullptr);
+        auto hr = raw->get_ProductName(&PtrVal);
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query AV product name.";
+            isGood = false;
+            return;
+        }
+
+        assert(PtrVal != nullptr);
+        name = std::wstring(PtrVal, SysStringLen(PtrVal));
+        SysFreeString(PtrVal);
+        PtrVal = nullptr;
+    }
+
+    void GetState(std::wstring& state)
+    {
+        assert(isGood);
+
+        auto ProductState = WSC_SECURITY_PRODUCT_STATE::WSC_SECURITY_PRODUCT_STATE_EXPIRED;
+        auto hr = raw->get_ProductState(&ProductState);
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query AV product state.";
+            isGood = false;
+            return;
+        }
+
+        switch (ProductState)
+        {
+        case WSC_SECURITY_PRODUCT_STATE_ON:
+            state = L"On";
+            break;
+        case WSC_SECURITY_PRODUCT_STATE_OFF:
+            state = L"Off";
+            break;
+        default:
+            // WSC_SECURITY_PRODUCT_STATE_SNOOZED - is this intentional?
+            // WSC_SECURITY_PRODUCT_STATE_EXPIRED
+            state = L"Expired";
+            break;
+        }
+    }
+
+    void GetDefinitionStatus(std::string& status)
+    {
+        assert(isGood);
+
+        auto ProductStatus = WSC_SECURITY_SIGNATURE_STATUS::WSC_SECURITY_PRODUCT_OUT_OF_DATE;
+        auto hr = raw->get_SignatureStatus(&ProductStatus);
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query AV product definition state.";
+            isGood = false;
+            return;
+        }
+
+        status = (ProductStatus == WSC_SECURITY_PRODUCT_UP_TO_DATE) ?
+            std::string("UpToDate") : std::string("OutOfDate");
+    }
+
+    void GetDefinitionUpdateTime(std::wstring& time)
+    {
+        assert(isGood);
+
+        auto PtrVal = BSTR(nullptr);
+        auto hr = raw->get_ProductStateTimestamp(&PtrVal);
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query AV product definition state.";
+            isGood = false;
+            return;
+        }
+
+        assert(PtrVal != nullptr);
+        time = std::wstring(PtrVal, SysStringLen(PtrVal));
+        SysFreeString(PtrVal);
+        PtrVal = nullptr;
+    }
+
+public:
+    Product(IWscProduct* product) : raw(product), isGood(true) {}
+
+    ~Product()
+    {
+        assert(isGood);
+        raw->Release();
+    }
+
+    void GetInfo(ThirdPartyAVSoftware& software)
+    {
+        assert(isGood);
+
+        if (isGood)
+        {
+            GetName(software.Name);
+        }
+
+        if (isGood)
+        {
+            GetState(software.ProductState);
+        }
+        software.Description = software.ProductState; // Is this intentional?
+
+        if (isGood)
+        {
+            GetDefinitionStatus(software.DefinitionStatus);
+        }
+
+        if (isGood)
+        {
+            GetDefinitionUpdateTime(software.DefinitionUpdateTime);
+        }
+    }
+
+    bool IsGood() const { return isGood; }
+};
+
+class ProductList
+{
+private:
+    IWSCProductList* raw; // RAII
+    long count;
+    bool isGood;
+
+    void CreateInstance()
+    {
+        auto hr = CoCreateInstance(__uuidof(WSCProductList), NULL, CLSCTX_INPROC_SERVER, __uuidof(IWSCProductList), reinterpret_cast<LPVOID*>(&raw));
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to create WSCProductList object. ";
+            isGood = false;
+        }
+
+        isGood = true;
+    }
+
+    void Init()
+    {
+        assert(isGood);
+
+        auto hr = raw->Initialize(WSC_SECURITY_PROVIDER_ANTIVIRUS);
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query antivirus product list. ";
+            isGood = false;
+        }
+    }
+
+    void GetTotalCount()
+    {
+        assert(isGood);
+
+        auto hr = raw->get_Count(&count);
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query product count.";
+            isGood = false;
+        }
+    }
+
+public:
+    ProductList() : raw(nullptr), count(-1), isGood(true)
+    {
+        CreateInstance();
+
+        if (isGood)
+        {
+            Init();
+        }
+
+        if (isGood)
+        {
+            GetTotalCount();
+        }
+    }
+
+    std::unique_ptr<Product> GetProduct(long i)
+    {
+        assert(isGood);
+
+        auto product = (IWscProduct*)nullptr;
+        auto hr = raw->get_Item(i, &product);
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query AV product.";
+            isGood = false;
+            return nullptr;
+        }
+
+        return std::make_unique<Product>(product);
+    }
+
+    long GetCount() const { return count; }
+    bool IsGood() const { return isGood; }
+};
 
 bool queryWindowsForAVSoftwareDataWSC(std::map<std::wstring, ThirdPartyAVSoftware>& thirdPartyAVSoftwareMap)
 {
-    auto PtrProductList = (IWSCProductList *)nullptr;
-
-    if (!GetProductList(&PtrProductList))
+    auto productList = std::make_unique<ProductList>();
+    if (!productList->IsGood())
     {
         return false;
     }
 
-    if (!InitializeProductList(PtrProductList))
-    {
-        return false;
-    }
-
-    auto ProductCount = 0L;
-    if (!GetProductListCount(PtrProductList, ProductCount))
-    {
-        return false;
-    }
-
-    for (auto i = 0L; i < ProductCount; ++i)
+    for (auto i = 0L; i < productList->GetCount(); ++i)
     {
         ThirdPartyAVSoftware thirdPartyAVSoftware;
-        auto PtrProduct = (IWscProduct *)nullptr;
-
-        if (GetProduct(PtrProductList, i, &PtrProduct) &&
-            GetProductInfo(PtrProduct, thirdPartyAVSoftware))
+        auto product = productList->GetProduct(i);
+        
+        if (product != nullptr)
         {
-            thirdPartyAVSoftwareMap[thirdPartyAVSoftware.Name] = thirdPartyAVSoftware;
+            product->GetInfo(thirdPartyAVSoftware);
+            if (product->IsGood())
+            {
+                thirdPartyAVSoftwareMap[thirdPartyAVSoftware.Name] = thirdPartyAVSoftware;
+            }
         }
-
-        PtrProduct->Release();
-        PtrProduct = nullptr;
     }
 
     return (thirdPartyAVSoftwareMap.size() > 0);
-}
-
-bool GetProductList(IWSCProductList** productList)
-{
-    auto hr = CoCreateInstance(__uuidof(WSCProductList), NULL, CLSCTX_INPROC_SERVER, __uuidof(IWSCProductList), reinterpret_cast<LPVOID*>(productList));
-    if (FAILED(hr))
-    {
-        std::cout << "Failed to create WSCProductList object. ";
-        return false;
-    }
-
-    assert(*productList != nullptr);
-    return true;
-}
-
-bool InitializeProductList(IWSCProductList* productList)
-{
-    assert(productList != nullptr);
-
-    auto hr = productList->Initialize(WSC_SECURITY_PROVIDER_ANTIVIRUS);
-    if (FAILED(hr))
-    {
-        std::cout << "Failed to query antivirus product list. ";
-        return false;
-    }
-
-    return true;
-}
-
-bool GetProductListCount(IWSCProductList* productList, long& count)
-{
-    assert(productList != nullptr);
-
-    auto hr = productList->get_Count(&count);
-    if (FAILED(hr))
-    {
-        std::cout << "Failed to query product count.";
-        return false;
-    }
-
-    return true;
-}
-
-bool GetProduct(IWSCProductList* productList, long i, IWscProduct** product)
-{
-    assert(productList != nullptr);
-
-    *product = nullptr;
-    auto hr = productList->get_Item(i, product);
-    if (FAILED(hr))
-    {
-        std::cout << "Failed to query AV product.";
-        return false;
-    }
-
-    assert(*product != nullptr);
-    return true;
-}
-
-bool GetProductInfo(IWscProduct* product, ThirdPartyAVSoftware& software)
-{
-    assert(product != nullptr);
-
-    if (!GetProductName(product, software.Name))
-    {
-        return false;
-    }
-
-    if (!GetProductState(product, software.ProductState))
-    {
-        return false;
-    }
-    software.Description = software.ProductState;
-
-    if (!GetDefinitionStatus(product, software.DefinitionStatus))
-    {
-        return false;
-    }
-
-    if (!GetDefinitionUpdateTime(product, software.DefinitionUpdateTime))
-    {
-        return false;
-    }
-
-    return true;
-}
-
-bool GetProductName(IWscProduct* product, std::wstring& outName)
-{
-    assert(product != nullptr);
-
-    auto PtrVal = BSTR(nullptr);
-    auto hr = product->get_ProductName(&PtrVal);
-    if (FAILED(hr))
-    {
-        std::cout << "Failed to query AV product name.";
-        return false;
-    }
-
-    assert(PtrVal != nullptr);
-    outName = std::wstring(PtrVal, SysStringLen(PtrVal));
-    SysFreeString(PtrVal);
-    PtrVal = nullptr;
-
-    return true;
-}
-
-bool GetProductState(IWscProduct* product, std::wstring& outState)
-{
-    assert(product != nullptr);
-
-    auto ProductState = WSC_SECURITY_PRODUCT_STATE::WSC_SECURITY_PRODUCT_STATE_EXPIRED;
-    auto hr = product->get_ProductState(&ProductState);
-    if (FAILED(hr))
-    {
-        std::cout << "Failed to query AV product state.";
-        return false;
-    }
-
-    switch (ProductState)
-    {
-    case WSC_SECURITY_PRODUCT_STATE_ON:
-        outState = L"On";
-        break;
-    case WSC_SECURITY_PRODUCT_STATE_OFF:
-        outState = L"Off";
-        break;
-    default:
-        // WSC_SECURITY_PRODUCT_STATE_SNOOZED - is this intentional?
-        // WSC_SECURITY_PRODUCT_STATE_EXPIRED
-        outState = L"Expired";
-        break;
-    }
-
-    return true;
-}
-
-bool GetDefinitionStatus(IWscProduct* product, std::string& outStatus)
-{
-    assert(product != nullptr);
-
-    auto ProductStatus = WSC_SECURITY_SIGNATURE_STATUS::WSC_SECURITY_PRODUCT_OUT_OF_DATE;
-    auto hr = product->get_SignatureStatus(&ProductStatus);
-    if (FAILED(hr))
-    {
-        std::cout << "Failed to query AV product definition state.";
-        return false;
-    }
-
-    outStatus = (ProductStatus == WSC_SECURITY_PRODUCT_UP_TO_DATE) ?
-        std::string("UpToDate") : std::string("OutOfDate");
-    return true;
-}
-
-bool GetDefinitionUpdateTime(IWscProduct* product, std::wstring& outTime)
-{
-    assert(product != nullptr);
-
-    auto PtrVal = BSTR(nullptr);
-    auto hr = product->get_ProductStateTimestamp(&PtrVal);
-    if (FAILED(hr))
-    {
-        std::cout << "Failed to query AV product definition state.";
-        return false;
-    }
-
-    assert(PtrVal != nullptr);
-    outTime = std::wstring(PtrVal, SysStringLen(PtrVal));
-    SysFreeString(PtrVal);
-    PtrVal = nullptr;
-
-    return true;
 }
--- step3/src/main.cpp
+++ step3/src/main.cpp
@@ -1,3 +1,7 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 #include <algorithm>
 #include <iostream>
 
@@ -5,33 +9,57 @@
 
 #include "CodeReview.h"
 
+bool Init();
+void DisplayAVSoftware();
+void Shutdown();
+
 int main(void)
+{
+    if (!Init())
+    {
+        return 1;
+    }
+
+    DisplayAVSoftware();
+    Shutdown();
+
+    _CrtDumpMemoryLeaks();
+    return 0;
+}
+
+bool Init()
 {
     auto hr = CoInitialize(0);
     if (FAILED(hr))
     {
         std::cout << "Failed to initialize COM library." << std::endl;
-        return 1;
+        return false;
     }
 
+    return true;
+}
+
+void DisplayAVSoftware()
+{
     auto thirdPartyAVSoftwareMap = std::map<std::wstring, ThirdPartyAVSoftware>{};
     if (queryWindowsForAVSoftwareDataWSC(thirdPartyAVSoftwareMap))
     {
         std::for_each(thirdPartyAVSoftwareMap.begin(), thirdPartyAVSoftwareMap.end(),
             [](const std::pair<std::wstring, ThirdPartyAVSoftware>& softwarePair)
-                {
-                    const auto software = softwarePair.second;
-                    std::wcout << software.Name << std::endl;
-                    std::wcout << software.Description << std::endl;
-                    std::wcout << software.DefinitionUpdateTime << std::endl;
-                    std::cout << software.DefinitionStatus << std::endl;
-                    //std::wcout << software.Version << std::endl;
-                    std::wcout << software.ProductState << std::endl;
-                    std::cout << "----" << std::endl;
-                });
+        {
+            const auto software = softwarePair.second;
+            std::wcout << software.Name << std::endl;
+            std::wcout << software.Description << std::endl;
+            std::wcout << software.DefinitionUpdateTime << std::endl;
+            std::cout << software.DefinitionStatus << std::endl;
+            //std::wcout << software.Version << std::endl;
+            std::wcout << software.ProductState << std::endl;
+            std::cout << "----" << std::endl;
+        });
     }
+}
 
+void Shutdown()
+{
     CoUninitialize();
-
-    return 0;
 }
