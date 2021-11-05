5f71692d926023f187a6d433047f7831add3dddb
Francis Joseph Serina <francis.serina@gmail.com>
2021-11-05 20:54:50 +0800

    Added answers to step 4

--- step4/CodeChallenge.cpp
+++ step4/CodeChallenge.cpp
@@ -50,6 +50,13 @@ T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 =
     return u.getResult(param1, param2, param3);
 }
 
+template <typename T, typename U>
+T checkIfPositive(const U& u, const int& param1, const int& param2, const int& param3, int& total)
+{
+    total = param1 + param2 + param3;
+    return checkIfPositive<T, U>(u, param1, param2, param3);
+}
+
 //<<<<<<<< END OF EDITABLE SECTION OF CODE <<<<<<<<<<<
 
 int main()
@@ -78,7 +85,7 @@ int main()
     //How are you going to solve this problem? 
     //You can only change the code inside the EDITABLE SECTION OF CODE
     //Hint, the following calls should now be valid moving forward:
-    /*
+    //*
     int total = 0;
 
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2);
@@ -98,7 +105,7 @@ int main()
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total); //<-- Total is output param
     std::cout << sResult << " " << total << std::endl;
-    */
+    //*/
 
     return 0;
 
