7be8a251c11dc6da0bccdcb1d1a57a2de403deb9
David Lynch <david.lynchmeister@gmail.com>
2022-02-12 19:06:11 -0600

    Step 2 code completion

--- step2/src/CodeStep2.cpp
+++ step2/src/CodeStep2.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 
 ////STRUCTURES START
 class Job
@@ -68,42 +70,71 @@ public:
 };
 ////STRUCTURES END
 
+////TEMPLATE FUNCTIONS START
 template <class T>
-void IsInBounds(T arg, T low_range, T high_range)
+bool IsInBounds(T arg, T low_range, T high_range)
 {
-    cout << "Input : " << arg;
     if ((low_range < arg) && (arg < high_range))
     {
-        cout << " is between ";
+        return true;
     }
     else
     {
-        cout << " is not between ";
+        return false;
     }
-    cout << low_range << " and " << high_range << endl;
 }
-////TEMPLATE FUNCTIONS START
+
+// Variadic template
+template <typename T, typename... Targs>
+void coutAll(T term1, Targs... nextTerm)
+{
+    cout << term1;
+    coutAll(nextTerm...);
+}
 
 ////TEMPLATE FUNCTIONS END
+
+////UTILITY FUNCTIONS START
+int ContainsTheString(bool (*testFunction)(const string &), vector<string> testdata)
+{
+    int result(0);
+    for (auto i : testdata)
+    {
+        if (testFunction(i))
+        { // we have a match!
+            ++result;
+        }
+    }
+    return result;
+}
+////UTILITY FUNCTIONS END
+
 int main()
 {
     // dynamic allocation
     Job *prg_job = new Programmer();
     Job *plt_job = new Pilot();
+    // my job collection
     Job *all_jobs[] = {prg_job, plt_job};
 
     for (auto i : all_jobs)
     {
-        cout << "Position: " << i->DisplayTitle() << endl;
-        cout << "Description: " << i->Describe() << endl;
-        cout << "Hours per week needed: " << i->WorkWeek() << endl;
+        coutAll("Position: ", i->DisplayTitle(), "\n", "Description: ", i->Describe(), "\n", "Hours per week needed: ", i->WorkWeek());
         i->DoWork();
         cout << endl; // space out each entry
     }
 
     // Test template function
     uint32_t httpResponseCode = 570;
-    IsInBounds<uint32_t>(httpResponseCode, 500, 599);
+    bool test_bounds_result = IsInBounds<uint32_t>(httpResponseCode, 500, 599);
+    cout << "Testing of IsInBounds for " << httpResponseCode << " within the bounds of 500 and 599 results in " << ((test_bounds_result) ? "IN BOUNDS" : "OUT OF BOUNDS") << endl;
+
+    // Strings vector test
+    auto theStrings = vector<std::string>{"one", "two", "test"};
+    auto count = ContainsTheString([](const std::string &tested)
+                                   { return tested == "test"; },
+                                   theStrings);
+    cout << "Testing of theStrings vector has resulted in " << count << " match(es)!" << endl;
 
     // memory cleanup
     delete prg_job;
