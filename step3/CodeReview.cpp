e1e142b5c2b05894e405fbf29667193806c011d4
folle <folle@outlook.com>
2022-06-03 09:01:54 -0300

    Step 3 answer.

--- step3/CodeReview.cpp
+++ step3/CodeReview.cpp
@@ -14,118 +14,183 @@ Comments are encouraged.
 struct ThirdPartyAVSoftware
 {
     std::wstring Name;
-    std::wstring Description;
+    // Removed the "Description" variable as it is being assigned the same value as ProductState.
     std::wstring DefinitionUpdateTime;
     std::string DefinitionStatus;
-    std::wstring Version;
+    // Removed the "Version" variable as it never gets used.
     std::wstring ProductState;
 };
 
 bool queryWindowsForAVSoftwareDataWSC(std::map<std::wstring, ThirdPartyAVSoftware>& thirdPartyAVSoftwareMap)
 {
+    // Moved the variables definitions closer to the first time they are actually used.
+    // Removed the "versionNumber" variable as it never gets used.
+
     HRESULT hr = S_OK;
-    IWscProduct* PtrProduct = nullptr;
-    IWSCProductList* PtrProductList = nullptr;
-    BSTR PtrVal = nullptr;
-    LONG ProductCount = 0;
-    WSC_SECURITY_PRODUCT_STATE ProductState;
-    WSC_SECURITY_SIGNATURE_STATUS ProductStatus;
-
-    std::wstring displayName, versionNumber, state, timestamp;
-    std::string definitionState;
-
-    hr = CoCreateInstance(__uuidof(WSCProductList), NULL, CLSCTX_INPROC_SERVER, __uuidof(IWSCProductList), reinterpret_cast<LPVOID*>(&PtrProductList));
-    if (FAILED(hr))
+    IWSCProductList* ptrProductList = nullptr; // Matched naming convention.
+    // Moved init statement inside if.
+    if (hr = CoCreateInstance(__uuidof(WSCProductList), NULL, CLSCTX_INPROC_SERVER, __uuidof(IWSCProductList), reinterpret_cast<LPVOID*>(&PtrProductList)); FAILED(hr))
     {
-        std::cout << "Failed to create WSCProductList object. ";
+        std::cout << "Failed to create WSCProductList object."; // Removed space at the end to keep consistency.
         return false;
     }
 
-    hr = PtrProductList->Initialize(WSC_SECURITY_PROVIDER_ANTIVIRUS);
-    if (FAILED(hr))
+    // Wraped IWSCProductList* into a std::unique_ptr with a custom deleter to avoid memory leaks and reduce the number of release calls.
+    auto productListDeleter = [](IWSCProductList* ptrProductList)
+                              {
+                                  if (ptrProductList == nullptr) { return; }
+
+                                  ptrProductList->Release(); // Added missing Release.
+                              };
+
+    std::unique_ptr<IWSCProductList, decltype(productListDeleter)> productList(ptrProductList, productListDeleter);
+
+    // Moved init statement inside if.
+    if (hr = productList->Initialize(WSC_SECURITY_PROVIDER_ANTIVIRUS); FAILED(hr))
     {
-        std::cout << "Failed to query antivirus product list. ";
+        std::cout << "Failed to query antivirus product list."; // Removed space at the end to keep consistency.
         return false;
     }
 
-    hr = PtrProductList->get_Count(&ProductCount);
-    if (FAILED(hr))
+    LONG productCount = 0; // Matched naming convention.
+    // Moved init statement inside if.
+    if (hr = productList->get_Count(&productCount); FAILED(hr))
     {
         std::cout << "Failed to query product count.";
         return false;
     }
 
-    for (uint32_t i = 0; i < ProductCount; i++)
+    // Fixed type mismatch. Use pre-increment instead of post-increment for i.
+    for (uint32_t i = 0; i < static_cast<uint32_t>(productCount); ++i)
     {
-        hr = PtrProductList->get_Item(i, &PtrProduct);
-        if (FAILED(hr))
+        IWscProduct* ptrProduct = nullptr; // Matched naming convention.
+        // Moved init statement inside if.
+        if (hr = productList->get_Item(i, &ptrProduct); FAILED(hr))
         {
             std::cout << "Failed to query AV product.";
             continue;
         }
 
-        hr = PtrProduct->get_ProductName(&PtrVal);
-        if (FAILED(hr))
+        // Wraped IWscProduct* into a std::unique_ptr with a custom deleter to avoid memory leaks and reduce the number of release calls.
+        auto productDeleter = [](IWscProduct* ptrProduct)
+                                {
+                                    if (ptrProduct == nullptr) { return; }
+
+                                    ptrProduct->Release();
+                                };
+
+        std::unique_ptr<IWscProduct, decltype(productDeleter)> product(ptrProduct, productDeleter);
+
+        BSTR ptrProductName = nullptr; // Better variable name.
+        // Moved init statement inside if.
+        if (hr = product->get_ProductName(&ptrProductName); FAILED(hr))
         {
-            PtrProduct->Release();
+            // Removed Release call as we have a unique_ptr with a custom deleter now.
             std::cout << "Failed to query AV product name.";
             continue;
         }
 
-        displayName = std::wstring(PtrVal, SysStringLen(PtrVal));
+        // Used const and parameterized constructor.
+        const std::wstring displayName(ptrProductName, SysStringLen(ptrProductName));
+        SysFreeString(ptrProductName); // Added missing SysFreeString. No point in creating a smart pointer out of it as we already have the value in the above wstring, so just free it.
 
-        hr = PtrProduct->get_ProductState(&ProductState);
-        if (FAILED(hr))
+        // Matched naming convention and initialized it to a default value. 'WSC_SECURITY_PRODUCT_STATE_OFF = 1' seems better than 'WSC_SECURITY_PRODUCT_STATE_ON = 0'.
+        WSC_SECURITY_PRODUCT_STATE productState = WSC_SECURITY_PRODUCT_STATE::WSC_SECURITY_PRODUCT_STATE_OFF;
+        if (hr = product->get_ProductState(&productState); FAILED(hr))
         {
             std::cout << "Failed to query AV product state.";
             continue;
         }
 
-        if (ProductState == WSC_SECURITY_PRODUCT_STATE_ON)
+        std::wstring state;
+        // Used switch instead of chained ifs.
+        switch (productState)
         {
-            state = L"On";
-        }
-        else if (ProductState == WSC_SECURITY_PRODUCT_STATE_OFF)
-        {
-            state = L"Off";
-        }
-        else
-        {
-            state = L"Expired";
+            case WSC_SECURITY_PRODUCT_STATE::WSC_SECURITY_PRODUCT_STATE_ON: // Added the enum name 'WSC_SECURITY_PRODUCT_STATE' before the value.
+            {
+                state = L"On";
+                break;
+            }
+            case WSC_SECURITY_PRODUCT_STATE::WSC_SECURITY_PRODUCT_STATE_OFF: // Added the enum name 'WSC_SECURITY_PRODUCT_STATE' before the value.
+            {
+                state = L"Off";
+                break;
+            }
+            case WSC_SECURITY_PRODUCT_STATE::WSC_SECURITY_PRODUCT_STATE_SNOOZED : // Added snoozed handling.
+            {
+                state = L"Snoozed";
+                break;
+            }
+            case WSC_SECURITY_PRODUCT_STATE::WSC_SECURITY_PRODUCT_STATE_EXPIRED : // Added dedicated expired handling
+            {
+                state = L"Expired";
+                break;
+            }
+            default: // Added unknown handling
+            {
+                state = L"Unknown";
+                break;
+            }
         }
 
-        hr = PtrProduct->get_SignatureStatus(&ProductStatus);
-        if (FAILED(hr))
+        WSC_SECURITY_SIGNATURE_STATUS productStatus _WSC_SECURITY_SIGNATURE_STATUS::WSC_SECURITY_PRODUCT_OUT_OF_DATE ; // Matched naming convention and initialized it to a default value. 
+        if (hr = product->get_SignatureStatus(&productStatus); FAILED(hr))
         {
             std::cout << "Failed to query AV product definition state.";
             continue;
         }
 
-        definitionState = (ProductStatus == WSC_SECURITY_PRODUCT_UP_TO_DATE) ? "UpToDate" : "OutOfDate";
-
-        hr = PtrProduct->get_ProductStateTimestamp(&PtrVal);
-        if (FAILED(hr))
+        std::string definitionState;
+        // Used switch instead of ternary operator.
+        switch (productStatus)
+        {
+            case _WSC_SECURITY_SIGNATURE_STATUS::WSC_SECURITY_PRODUCT_STATE_ON: // Added the enum name '_WSC_SECURITY_SIGNATURE_STATUS' before the value.
+            {
+                definitionState = "UpToDate";
+                break;
+            }
+            case _WSC_SECURITY_SIGNATURE_STATUS::WSC_SECURITY_PRODUCT_STATE_OFF: // Added the enum name '_WSC_SECURITY_SIGNATURE_STATUS' before the value.
+            {
+                definitionState = "OutOfDate";
+                break;
+            }
+            default: // Added unknown handling
+            {
+                definitionState = "Unknown";
+                break;
+            }
+        }
+        
+        BSTR ptrTimestamp = nullptr; // Better variable name.
+        // Moved init statement inside if.
+        if (hr = product->get_ProductStateTimestamp(&ptrTimestamp); FAILED(hr))
         {
             std::cout << "Failed to query AV product definition state.";
             continue;
         }
-        timestamp = std::wstring(PtrVal, SysStringLen(PtrVal));
-        SysFreeString(PtrVal);
 
-        ThirdPartyAVSoftware thirdPartyAVSoftware;
-        thirdPartyAVSoftware.Name = displayName;
-        thirdPartyAVSoftware.DefinitionStatus = definitionState;
-        thirdPartyAVSoftware.DefinitionUpdateTime = timestamp;
+        // Used const and parameterized constructor.
+        const std::wstring timestamp(ptrTimestamp, SysStringLen(ptrTimestamp));
+        SysFreeString(ptrTimestamp); // No point in creating a smart pointer out of it as we already have the value in the above wstring, so just free it.
+
+        // Use bracket init as any new members will be default initialized/constructed.
+        ThirdPartyAVSoftware thirdPartyAVSoftware{};
+        thirdPartyAVSoftware.Name = std::move(displayName); // Use move.
+        thirdPartyAVSoftware.DefinitionStatus = std::move(definitionState); // Use move.
+        thirdPartyAVSoftware.DefinitionUpdateTime = std::move(timestamp); // Use move.
         thirdPartyAVSoftware.Description = state;
-        thirdPartyAVSoftware.ProductState = state;
-        thirdPartyAVSoftwareMap[thirdPartyAVSoftware.Name] = thirdPartyAVSoftware;
+        thirdPartyAVSoftware.ProductState = std::move(state); // Use move.
 
-        PtrProduct->Release();
+        thirdPartyAVSoftwareMap[thirdPartyAVSoftware.Name] = std::move(thirdPartyAVSoftware); // Use move.
+
+        // Removed Release call as we have a unique_ptr with a custom deleter now.
     }
 
-    if (thirdPartyAVSoftwareMap.size() == 0)
+    // Check empty instead of size == 0.
+    if (thirdPartyAVSoftwareMap.empty())
     {
         return false;
     }
+
     return true;
 }
