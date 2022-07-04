8ba9ebbf6164d757d44bb6e280f521556e25a10a
rodrigocanellas <rodrigo.canellas@gmail.com>
2022-07-04 20:28:37 -0300

    Finishing 'step1' and improving documentation of 'step2'

--- step2/src/main.cpp
+++ step2/src/main.cpp
@@ -9,21 +9,38 @@
 #include <string>
 #include <vector>
 
+/// \brief Namespace to group code related to jobs classes and functions
 namespace jobs {
 
+/// \brief Type parameterized base class for jobs
+///
+/// \tparam NAME is a string like type used to hold the name of the job
+///
+/// \tparam DESCRIPTION is a string like type used to hold the description of
+/// the job
+///
+/// \tparam HOURS is a number like type to hold the number of hours necessary
+/// for the job
 template <typename NAME, typename DESCRIPTION, typename HOURS> class Job_t {
 public:
   using Name = NAME;
   using Description = DESCRIPTION;
   using Hours = HOURS;
 
+  /// \brief Pointer type for jobs types
   using Ptr = std::shared_ptr<Job_t<NAME, DESCRIPTION, HOURS>>;
 
 public:
+  /// \brief Job_t types can not be created without attribute definition
   Job_t() = delete;
+
+  /// \brief Job_t types can not be copied
   Job_t(const Job_t &) = delete;
+
+  /// \brief Job_t types can not be moved
   Job_t(Job_t &&) = delete;
 
+  /// \brief Job_t types can only be dinamically created
   template <typename JOB> static Ptr create(Hours pHours) {
     return std::make_shared<JOB>(pHours);
   }
@@ -37,10 +54,16 @@ public:
     out << "My work involves " << mDescription << '\n';
   }
 
+  /// \brief Job_t types can not be copied
   Job_t &operator=(const Job_t &) = delete;
+
+  /// \brief Job_t types can not be moved
   Job_t &operator=(Job_t &&) = delete;
 
+  virtual ~Job_t() = default;
+
 protected:
+  /// \brief Protected constructor to be used by sub-classes
   Job_t(Name &&pName, Description &&pDescription, Hours pHours)
       : mName(std::move(pName)), mDescription(std::move(pDescription)),
         mHours(pHours) {}
@@ -51,31 +74,54 @@ protected:
   Hours mHours;
 };
 
+/// \brief Programmer job type
+///
+/// \tparam NAME is a string like type used to hold the name of the job
+///
+/// \tparam DESCRIPTION is a string like type used to hold the description of
+/// the job
+///
+/// \tparam HOURS is a number like type to hold the number of hours necessary
+/// for the job
 template <typename NAME, typename DESCRIPTION, typename HOURS>
 class Programmer_t : public Job_t<NAME, DESCRIPTION, HOURS> {
 public:
+  /// \brief The amount of hours can be freely defined
   Programmer_t(HOURS pHours)
       : Job_t<NAME, DESCRIPTION, HOURS>("Programmer",
                                         "Writes ninja level programs", pHours) {
   }
 };
 
+/// \brief Pilot job type
+///
+/// \tparam NAME is a string like type used to hold the name of the job
+///
+/// \tparam DESCRIPTION is a string like type used to hold the description of
+/// the job
+///
+/// \tparam HOURS is a number like type to hold the number of hours necessary
+/// for the job
 template <typename NAME, typename DESCRIPTION, typename HOURS>
 class Pilot_t : public Job_t<NAME, DESCRIPTION, HOURS> {
 public:
+  /// \brief The amount of hours can be freely defined
   Pilot_t(HOURS pHours)
       : Job_t<NAME, DESCRIPTION, HOURS>("Pilot", "Flies all over the world",
                                         pHours) {}
 };
 
+/// \brief Type of collection of (pointer to) jobs
 template <typename NAME, typename DESCRIPTION, typename HOURS>
 using Jobs_t = std::vector<typename Job_t<NAME, DESCRIPTION, HOURS>::Ptr>;
 
+/// \brief Helper function to add jobs to a collection of jobs
 template <typename JOBS, typename JOB>
 void add_job(JOBS &pJobs, typename JOB::Hours hours) {
   pJobs.push_back(JOB::template create<JOB>(hours));
 }
 
+/// \brief Helper function to traverse a collection of jobs
 template <typename JOBS, typename JOB_PTR>
 void traverse_jobs(JOBS &pJobs, std::function<void(JOB_PTR)> visit) {
 
@@ -85,6 +131,7 @@ void traverse_jobs(JOBS &pJobs, std::function<void(JOB_PTR)> visit) {
 }
 } // namespace jobs
 
+/// \brief A out of context class, to demonstrate template based functions
 class Color {
 public:
   Color(uint8_t r, uint8_t g, uint8_t b) : red(r), green(g), blue(b) {}
@@ -119,22 +166,36 @@ private:
   uint8_t blue;
 };
 
+/// \brief Namespace for bound related functions
 namespace bounds {
 
-template <typename t_value = uint32_t>
-bool IsInBounds(t_value value, t_value min, t_value max) {
+/// \brief Checks is a value is between two others
+///
+/// \tparam TYPE must implment >= and <=
+template <typename TYPE = uint32_t>
+bool IsInBounds(TYPE &&value, TYPE &&min, TYPE &&max) {
   return ((value >= min) && (value <= max));
 }
 
-template <typename t_value = uint32_t>
-void TestIsInBounds(t_value value, t_value min, t_value max) {
+/// \brief Helper function to test
+template <typename TYPE = uint32_t>
+void TestIsInBounds(TYPE &&value, TYPE &&min, TYPE &&max) {
   std::cout << "Is " << value << " between " << min << " and " << max << "? "
-            << (IsInBounds(value, min, max) ? "yes" : "no") << '\n';
+            << (IsInBounds(std::forward<TYPE>(value), std::forward<TYPE>(min),
+                           std::forward<TYPE>(max))
+                    ? "yes"
+                    : "no")
+            << '\n';
 }
 } // namespace bounds
 
+/// \brief Namespace grouping function for the 'contains' part of the exercise
 namespace contains {
 
+/// \brief Helper function to print a sequence of values
+///
+/// \tparam ITERATOR is a iterator like type, and the type referenced must
+/// implement the << operator
 template <typename ITERATOR>
 void Print(ITERATOR begin, ITERATOR end, std::ostream &out = std::cout) {
   out << '[';
@@ -147,6 +208,10 @@ void Print(ITERATOR begin, ITERATOR end, std::ostream &out = std::cout) {
   out << ']';
 }
 
+/// \brief Compare function object
+///
+/// \tparam TYPE is the type of the value to be compared, it must implement
+/// operator ==
 template <typename TYPE> class Compare {
 public:
   Compare() = delete;
@@ -160,6 +225,18 @@ public:
   TYPE value;
 };
 
+/// \brief Determines if a sequence of values contains a value
+///
+/// \tparam ITERATOR is a iterator like to the value
+///
+/// \param compare is the function object used to compare values
+///
+/// \param begin is the begining of the sequence of values
+///
+/// \param end is the end of the sequence of values
+///
+/// \return the number of times the value defined in \p compare is found between
+/// \p begin and \p end
 template <typename ITERATOR>
 uint16_t ContainsTheValue(Compare<typename ITERATOR::value_type> &&compare,
                           ITERATOR begin, ITERATOR end) {
@@ -172,6 +249,7 @@ uint16_t ContainsTheValue(Compare<typename ITERATOR::value_type> &&compare,
   return count;
 }
 
+/// \brief Helper function to test \p ContainsTheValue
 template <typename ITERATOR>
 void TestContainsTheValue(ITERATOR begin, ITERATOR end,
                           const typename ITERATOR::value_type &tested,
@@ -184,6 +262,7 @@ void TestContainsTheValue(ITERATOR begin, ITERATOR end,
       << '\n';
 }
 
+/// \brief Helper function to test \p ContainsTheValue
 template <typename ITERATOR>
 void TestContainsTheValue(ITERATOR begin, ITERATOR end,
                           typename ITERATOR::value_type &&tested,
@@ -196,6 +275,7 @@ void TestContainsTheValue(ITERATOR begin, ITERATOR end,
       << '\n';
 }
 
+/// \brief Helper function to test \p ContainsTheValue
 template <typename CONTAINER>
 inline void TestContainsTheValue(const CONTAINER &container,
                                  typename CONTAINER::value_type &&tested,
@@ -205,6 +285,7 @@ inline void TestContainsTheValue(const CONTAINER &container,
                        std::forward<value_type>(tested), out);
 }
 
+/// \brief Helper function to test \p ContainsTheValue
 template <typename CONTAINER>
 inline void TestContainsTheValue(const CONTAINER &container,
                                  const typename CONTAINER::value_type &tested,
@@ -212,6 +293,7 @@ inline void TestContainsTheValue(const CONTAINER &container,
   TestContainsTheValue(container.begin(), container.end(), tested, out);
 }
 
+/// \brief Helper function to test \p ContainsTheValue
 template <typename CONTAINER>
 inline void TestContainsTheValue(CONTAINER &&container,
                                  const typename CONTAINER::value_type &tested,
@@ -219,6 +301,7 @@ inline void TestContainsTheValue(CONTAINER &&container,
   TestContainsTheValue(container.begin(), container.end(), tested, out);
 }
 
+/// \brief Helper function to test \p ContainsTheValue
 template <typename CONTAINER>
 inline void TestContainsTheValue(CONTAINER &&container,
                                  typename CONTAINER::value_type &&tested,
