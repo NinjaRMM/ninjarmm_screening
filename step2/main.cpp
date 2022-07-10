6c3d4032640008bd616cc6b342f8db4e5a6f3531
LeberechtReinhold <aitor@ajimenez.es>
2022-07-10 21:22:17 +0200

    Step 2 program, requirement i

--- step2/main.cpp
+++ step2/main.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 #include <iostream>
 #include <memory>
 #include <vector>
@@ -88,6 +89,19 @@ template <class T> bool IsInBounds(T value, T min, T max)
     return value >= min && value <= max;
 }
 
+unsigned int stringsMatchesTest(const std::function<bool(std::string)> test, const std::vector<std::string> strings)
+{
+    unsigned int count = 0;
+    for (const auto &str : strings)
+    {
+        if (test(str))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
 int main(int, char **)
 {
     std::cout << "Step 2, First exercise, requirements a to g: " << std::endl;
@@ -102,12 +116,28 @@ int main(int, char **)
         job->DoWork();
         std::cout << std::endl;
     }
-    
 
     std::cout << "Step 2, Second exercise, requirement h: " << std::endl;
     unsigned int httpResonseCode = 404;
     unsigned int minResponseCode = 500;
     unsigned int maxResponseCode = 599;
-    std::cout << "Value: " << httpResonseCode << ", min: " << minResponseCode << ", max: " << maxResponseCode << std::endl;
+    std::cout << "Value: " << httpResonseCode << ", min: " << minResponseCode << ", max: " << maxResponseCode
+              << std::endl;
     std::cout << "Is in Bounds: " << IsInBounds<uint32_t>(httpResonseCode, 500, 599) << std::endl;
+
+    std::cout << "Step 2, Third exercise, requirement i: " << std::endl;
+    auto theStrings = std::vector<std::string>{"one", "two", "test"};
+    auto count = stringsMatchesTest([](const std::string &tested) { return tested == "test"; }, theStrings);
+    if (count == 0)
+    {
+        std::cout << "None have matched the test";
+    }
+    else if (count == 1)
+    {
+        std::cout << count << " has matched the test";
+    }
+    else
+    {
+        std::cout << count << " have matched the test";
+    }
 }
