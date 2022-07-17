0a50e676e496e58839bc809222ecc4ba39a93864
unknown <netomalcher@gmail.com>
2022-07-17 16:36:28 -0300

    adding cpp file

---
+++ step3/src/ThirdPartyAVSoftware.cpp
@@ -0,0 +1,159 @@
+/*
+
+NINJARMM Code Review
+
+Please review the below code.
+We do not expect you to execute this code, but you are welcome to try.
+
+Make any code updates that you see fit (If any).
+Comments are encouraged.
+
+*/
+
+// Adding the missing headers for the code to compile
+#include <iostream>
+#include <string>
+#include <map>
+#include <windows.h>
+#include <iwscapi.h>
+#include <wscapi.h>
+
+
+struct ThirdPartyAVSoftware
+{
+    std::wstring Name;
+    std::wstring Description;
+    std::wstring DefinitionUpdateTime;
+    std::string DefinitionStatus;
+    std::wstring Version;
+    std::wstring ProductState;
+};
+
+bool queryWindowsForAVSoftwareDataWSC(std::map<std::wstring, ThirdPartyAVSoftware>& thirdPartyAVSoftwareMap)
+{
+    HRESULT hr = S_OK;
+    IWscProduct* PtrProduct = nullptr;
+    IWSCProductList* PtrProductList = nullptr;
+    BSTR PtrVal = nullptr;
+    LONG ProductCount = 0;
+    WSC_SECURITY_PRODUCT_STATE ProductState;
+    WSC_SECURITY_SIGNATURE_STATUS ProductStatus;
+
+    std::wstring displayName, versionNumber, state, timestamp;
+    std::string definitionState;
+
+    // Creating WSCProductList object
+    hr = CoCreateInstance(__uuidof(WSCProductList), NULL, CLSCTX_INPROC_SERVER, __uuidof(IWSCProductList), reinterpret_cast<LPVOID*>(&PtrProductList));
+    if (FAILED(hr))
+    {
+        std::cout << "Failed to create WSCProductList object. ";
+        return false;
+    }
+
+    // Querying antivirus product list
+    hr = PtrProductList->Initialize(WSC_SECURITY_PROVIDER_ANTIVIRUS);
+    if (FAILED(hr))
+    {
+        std::cout << "Failed to query antivirus product list. ";
+        return false;
+    }
+
+    // Querying product count
+    hr = PtrProductList->get_Count(&ProductCount);
+    if (FAILED(hr))
+    {
+        std::cout << "Failed to query product count.";
+        return false;
+    }
+
+    // Iterating through all the products
+    for (uint32_t i = 0; i < ProductCount; i++)
+    {
+        // Querying AV product
+        hr = PtrProductList->get_Item(i, &PtrProduct);
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query AV product.";
+            continue;
+        }
+
+        // Querying AV product name 
+        hr = PtrProduct->get_ProductName(&PtrVal);
+        if (FAILED(hr))
+        {
+            PtrProduct->Release();
+            std::cout << "Failed to query AV product name.";
+            continue;
+        }
+
+        // Displaying the AV product name
+        displayName = std::wstring(PtrVal, SysStringLen(PtrVal));
+
+        // Querying AV product state
+        hr = PtrProduct->get_ProductState(&ProductState);
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query AV product state.";
+            continue;
+        }
+
+        // Setting the product state to on, off or expired
+        if (ProductState == WSC_SECURITY_PRODUCT_STATE_ON)
+        {
+            state = L"On";
+        }
+        else if (ProductState == WSC_SECURITY_PRODUCT_STATE_OFF)
+        {
+            state = L"Off";
+        }
+        else
+        {
+            state = L"Expired";
+        }
+
+        // Querying AV product definition state
+        hr = PtrProduct->get_SignatureStatus(&ProductStatus);
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query AV product definition state.";
+            continue;
+        }
+
+        // Checking if the product is up-to-date or out of date
+        definitionState = (ProductStatus == WSC_SECURITY_PRODUCT_UP_TO_DATE) ? "UpToDate" : "OutOfDate";
+
+        // Querying for AV product definition state
+        hr = PtrProduct->get_ProductStateTimestamp(&PtrVal);
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query AV product definition state.";
+            continue;
+        }
+        timestamp = std::wstring(PtrVal, SysStringLen(PtrVal));
+        SysFreeString(PtrVal);
+
+        // Creating and filling the ThirdPartyAVSoftware structure with it's values 
+        ThirdPartyAVSoftware thirdPartyAVSoftware;
+        thirdPartyAVSoftware.Name = displayName;
+        thirdPartyAVSoftware.DefinitionStatus = definitionState;
+        thirdPartyAVSoftware.DefinitionUpdateTime = timestamp;
+        thirdPartyAVSoftware.Description = state;
+        thirdPartyAVSoftware.ProductState = state;
+        thirdPartyAVSoftwareMap[thirdPartyAVSoftware.Name] = thirdPartyAVSoftware;
+
+        PtrProduct->Release();
+    }
+
+    // Checking if the map is empty
+    if (thirdPartyAVSoftwareMap.size() == 0)
+    {
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+   // queryWindowsForAVSoftwareDataWSC(std::map<std::wstring,thirdPartyAVSoftwareMap)
+    return 0;
+}
