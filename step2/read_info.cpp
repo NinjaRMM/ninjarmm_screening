16cc9d24c03644ae08849287391edc88708c963d
fcatho <fcatho@gmail.com>
2022-08-12 08:31:22 -0300

    Changing tabs for spaces

--- step2/read_info.cpp
+++ step2/read_info.cpp
@@ -10,68 +10,68 @@
 void system_info()
 {
 #ifdef __linux__
-	struct sysinfo info;
-	::sysinfo(&info);
+    struct sysinfo info;
+    ::sysinfo(&info);
 
-	constexpr std::uint64_t seconds_per_day = 24 * 60 * 60;
-	constexpr std::uint64_t mb_factor = 1 << 20;
+    constexpr std::uint64_t seconds_per_day = 24 * 60 * 60;
+    constexpr std::uint64_t mb_factor = 1 << 20;
 
-	std::cout << "uptime: " << info.uptime / seconds_per_day << std::endl;
-	std::cout << "total ram (MB): " << info.totalram / mb_factor << std::endl;
-	std::cout << "free ram (MB): " << info.freeram / mb_factor << std::endl;
-	std::cout << "total swap (MB): " << info.totalswap / mb_factor << std::endl;
-	std::cout << "free swap (MB): " << info.freeswap / mb_factor << std::endl;
-	std::cout << "process running: " << info.procs << std::endl;
+    std::cout << "uptime: " << info.uptime / seconds_per_day << std::endl;
+    std::cout << "total ram (MB): " << info.totalram / mb_factor << std::endl;
+    std::cout << "free ram (MB): " << info.freeram / mb_factor << std::endl;
+    std::cout << "total swap (MB): " << info.totalswap / mb_factor << std::endl;
+    std::cout << "free swap (MB): " << info.freeswap / mb_factor << std::endl;
+    std::cout << "process running: " << info.procs << std::endl;
 
-	auto read_sys_file = [](std::string const & info, std::string const & file)
-	{
-		std::ifstream ifile(file);
-		if (ifile.good())
-		{
-			std::stringstream ss;
-			ss << ifile.rdbuf();
-			std::string text;
-			std::getline(ss, text);
-			std::cout << info << ": " << text << std::endl;
-		}
-	};
+    auto read_sys_file = [](std::string const & info, std::string const & file)
+    {
+        std::ifstream ifile(file);
+        if (ifile.good())
+        {
+            std::stringstream ss;
+            ss << ifile.rdbuf();
+            std::string text;
+            std::getline(ss, text);
+            std::cout << info << ": " << text << std::endl;
+        }
+    };
 
-	read_sys_file("machine hostname",                "/proc/sys/kernel/hostname");
-	read_sys_file("kernel version",                  "/proc/sys/kernel/osrelease");
-	read_sys_file("os type",                         "/proc/sys/kernel/ostype");
-	read_sys_file("boot unique id identifier",       "/proc/sys/kernel/random/boot_id");
-	read_sys_file("date which kernel was compiled",  "/proc/sys/kernel/version");
-	read_sys_file("sysrq enabled (emergency keys)",  "/proc/sys/kernel/sysrq");
-	read_sys_file("max number of cgroups namespaces", "/proc/sys/user/max_cgroup_namespaces");
-	read_sys_file("max number of mount namespaces",   "/proc/sys/user/max_net_namespaces");
+    read_sys_file("machine hostname",                "/proc/sys/kernel/hostname");
+    read_sys_file("kernel version",                  "/proc/sys/kernel/osrelease");
+    read_sys_file("os type",                         "/proc/sys/kernel/ostype");
+    read_sys_file("boot unique id identifier",       "/proc/sys/kernel/random/boot_id");
+    read_sys_file("date which kernel was compiled",  "/proc/sys/kernel/version");
+    read_sys_file("sysrq enabled (emergency keys)",  "/proc/sys/kernel/sysrq");
+    read_sys_file("max number of cgroups namespaces", "/proc/sys/user/max_cgroup_namespaces");
+    read_sys_file("max number of mount namespaces",   "/proc/sys/user/max_net_namespaces");
 #endif
 }
 
 void centos_patches()
 {
-	std::ifstream ifile("/etc/centos-release");
-	if (!ifile.good())
-	{
-		return;
-	}
+    std::ifstream ifile("/etc/centos-release");
+    if (!ifile.good())
+    {
+        return;
+    }
 
-	std::string cmd("rpm -q kernel --last");
-	FILE * pipe = popen(cmd.c_str(), "r");
+    std::string cmd("rpm -q kernel --last");
+    FILE * pipe = popen(cmd.c_str(), "r");
 
-	if (!pipe)
-	{
-		return;
-	}
+    if (!pipe)
+    {
+        return;
+    }
 
-	char buffer[1024];
-	memset(buffer, 0, sizeof(buffer));
+    char buffer[1024];
+    memset(buffer, 0, sizeof(buffer));
 
-	while(fgets(buffer, sizeof(buffer), pipe))
-	{
-		std::cout << buffer;
-	}
+    while(fgets(buffer, sizeof(buffer), pipe))
+    {
+        std::cout << buffer;
+    }
 
-	pclose(pipe);
+    pclose(pipe);
 }
 
 /*!
@@ -81,6 +81,6 @@ void centos_patches()
  */
 int main()
 {
-	system_info();
-	centos_patches();
+    system_info();
+    centos_patches();
 }
