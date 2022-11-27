533243726314ec8a1ba4a8b236751ba5d30d776d
Eduardo Fernandes <ceduardodfernandes@gmail.com>
2022-11-27 10:59:12 +0000

    Question 3

---
+++ step1/virtual_destructor_correct.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+class A {
+    public:
+    A() {
+        std::cout << "Constructing A\n";
+    }
+    virtual ~A() {
+        std::cout << "Destructing A\n";
+    }
+    
+};
+
+class B : public A {
+    public:
+    B() {
+        std::cout << "Constructing B\n";
+    }
+    ~B() {
+        std::cout << "Destructing B\n";
+    }
+};
+
+int main()
+{
+  std::string name;
+  B* b = new B();
+  A* a = new B();
+  
+  delete b;
+  delete a; //The destructor of B and A are called as ins the output bellow
+}
+
+/* Output
+Constructing A
+Constructing B
+Constructing A
+Constructing B
+Destructing B
+Destructing A
+Destructing B
+Destructing A
+*/
---
+++ step1/virtual_destructor_wrong.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+class A {
+    public:
+    A() {
+        std::cout << "Constructing A\n";
+    }
+    ~A() {
+        std::cout << "Destructing A\n";
+    }
+    
+};
+
+class B : public A {
+    public:
+    B() {
+        std::cout << "Constructing B\n";
+    }
+    ~B() {
+        std::cout << "Destructing B\n";
+    }
+};
+
+int main()
+{
+  std::string name;
+  B* b = new B();
+  A* a = new B();
+  
+  delete b;
+  delete a; //Only the destructor of A is called, althought the constructor of B is called what may cause memory leak, as shown in the output bellow
+}
+
+/* Output
+Constructing A
+Constructing B
+Constructing A
+Constructing B
+Destructing B
+Destructing A
+Destructing A
+*/
