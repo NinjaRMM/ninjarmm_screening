c54b18df750ee81e39cdb39de9bb515762dcd753
Leandro Dos Santos Ferreira <feslean@gmail.com>
2022-12-05 12:28:10 -0300

    Update CodeReview.cpp

--- step4/CodeReview.cpp
+++ step4/CodeReview.cpp
@@ -10,61 +10,68 @@ Comments are encouraged.
 
 */
 
+// Includes added
+
+#include <iostream>
+#include <string>
+#include <map>
 
 struct ThirdPartyAVSoftware
 {
-    std::wstring Name;
+    std::wstring Name; // Probably doesn't need wstring type
     std::wstring Description;
-    std::wstring DefinitionUpdateTime;
+    std::wstring DefinitionUpdateTime; // Maybe another type of data
     std::string DefinitionStatus;
-    std::wstring Version;
+    std::wstring Version; // Definitely doesn't need wstring
     std::wstring ProductState;
 };
 
-bool queryWindowsForAVSoftwareDataWSC(std::map<std::wstring, ThirdPartyAVSoftware>& thirdPartyAVSoftwareMap)
+
+
+bool queryWindowsForAVSoftwareDataWSC(std::map<std::wstring, ThirdPartyAVSoftware>& thirdPartyAVSoftwareMap) // Name of the function is too lengthy
 {
-    HRESULT hr = S_OK;
-    IWscProduct* PtrProduct = nullptr;
-    IWSCProductList* PtrProductList = nullptr;
-    BSTR PtrVal = nullptr;
-    LONG ProductCount = 0;
-    WSC_SECURITY_PRODUCT_STATE ProductState;
-    WSC_SECURITY_SIGNATURE_STATUS ProductStatus;
+    HRESULT hr = S_OK; // rvalue is invalid, might be missing a definition, lvalue is missing a definition
+    IWscProduct* PtrProduct = nullptr; // lvalue is missing a definition
+    IWSCProductList* PtrProductList = nullptr; // lvalue is missing a definition
+    BSTR PtrVal = nullptr; // lvalue is missing a definition
+    LONG ProductCount = 0; // LONG might be changed to primitive value
+    WSC_SECURITY_PRODUCT_STATE ProductState; // undefined type and missing variable initialization 
+    WSC_SECURITY_SIGNATURE_STATUS ProductStatus; // undefined type and missing variable initialization 
 
     std::wstring displayName, versionNumber, state, timestamp;
-    std::string definitionState;
+    std::string definitionState; // wrong type according to previous definition
 
-    hr = CoCreateInstance(__uuidof(WSCProductList), NULL, CLSCTX_INPROC_SERVER, __uuidof(IWSCProductList), reinterpret_cast<LPVOID*>(&PtrProductList));
-    if (FAILED(hr))
+    hr = CoCreateInstance(__uuidof(WSCProductList), NULL, CLSCTX_INPROC_SERVER, __uuidof(IWSCProductList), reinterpret_cast<LPVOID*>(&PtrProductList)); // undeclared function, missing header maybe
+    if (FAILED(hr)) // missing definition for FAILED
     {
-        std::cout << "Failed to create WSCProductList object. ";
+        std::cout << "Failed to create WSCProductList object. "; // add an endl here perhaps
         return false;
     }
 
-    hr = PtrProductList->Initialize(WSC_SECURITY_PROVIDER_ANTIVIRUS);
+    hr = PtrProductList->Initialize(WSC_SECURITY_PROVIDER_ANTIVIRUS);// null pointer
     if (FAILED(hr))
     {
-        std::cout << "Failed to query antivirus product list. ";
+        std::cout << "Failed to query antivirus product list. "; // add an endl here perhaps
         return false;
     }
 
-    hr = PtrProductList->get_Count(&ProductCount);
+    hr = PtrProductList->get_Count(&ProductCount); // null pointer
     if (FAILED(hr))
     {
-        std::cout << "Failed to query product count.";
+        std::cout << "Failed to query product count."; // add an endl here perhaps
         return false;
     }
 
-    for (uint32_t i = 0; i < ProductCount; i++)
+    for (uint32_t i = 0; i < ProductCount; i++) // should use an iterator
     {
-        hr = PtrProductList->get_Item(i, &PtrProduct);
+        hr = PtrProductList->get_Item(i, &PtrProduct); // null pointer
         if (FAILED(hr))
         {
             std::cout << "Failed to query AV product.";
             continue;
         }
 
-        hr = PtrProduct->get_ProductName(&PtrVal);
+        hr = PtrProduct->get_ProductName(&PtrVal); // null pointer
         if (FAILED(hr))
         {
             PtrProduct->Release();
@@ -72,7 +79,7 @@ bool queryWindowsForAVSoftwareDataWSC(std::map<std::wstring, ThirdPartyAVSoftwar
             continue;
         }
 
-        displayName = std::wstring(PtrVal, SysStringLen(PtrVal));
+        displayName = std::wstring(PtrVal, SysStringLen(PtrVal)); // null pointer
 
         hr = PtrProduct->get_ProductState(&ProductState);
         if (FAILED(hr))
@@ -83,41 +90,41 @@ bool queryWindowsForAVSoftwareDataWSC(std::map<std::wstring, ThirdPartyAVSoftwar
 
         if (ProductState == WSC_SECURITY_PRODUCT_STATE_ON)
         {
-            state = L"On";
+            state = L"On"; // invalid rvalue
         }
         else if (ProductState == WSC_SECURITY_PRODUCT_STATE_OFF)
         {
-            state = L"Off";
+            state = L"Off"; // invalid rvalue
         }
         else
         {
-            state = L"Expired";
+            state = L"Expired"; // invalid rvalue
         }
 
-        hr = PtrProduct->get_SignatureStatus(&ProductStatus);
+        hr = PtrProduct->get_SignatureStatus(&ProductStatus); // null pointer
         if (FAILED(hr))
         {
             std::cout << "Failed to query AV product definition state.";
             continue;
         }
 
-        definitionState = (ProductStatus == WSC_SECURITY_PRODUCT_UP_TO_DATE) ? "UpToDate" : "OutOfDate";
+        definitionState = (ProductStatus == WSC_SECURITY_PRODUCT_UP_TO_DATE) ? "UpToDate" : "OutOfDate"; // undefined rvalue
 
-        hr = PtrProduct->get_ProductStateTimestamp(&PtrVal);
+        hr = PtrProduct->get_ProductStateTimestamp(&PtrVal); // null pointer
         if (FAILED(hr))
         {
             std::cout << "Failed to query AV product definition state.";
             continue;
         }
-        timestamp = std::wstring(PtrVal, SysStringLen(PtrVal));
+        timestamp = std::wstring(PtrVal, SysStringLen(PtrVal)); // invalid constructor
         SysFreeString(PtrVal);
 
-        ThirdPartyAVSoftware thirdPartyAVSoftware;
+        ThirdPartyAVSoftware thirdPartyAVSoftware; // confusing name
         thirdPartyAVSoftware.Name = displayName;
         thirdPartyAVSoftware.DefinitionStatus = definitionState;
         thirdPartyAVSoftware.DefinitionUpdateTime = timestamp;
-        thirdPartyAVSoftware.Description = state;
-        thirdPartyAVSoftware.ProductState = state;
+        thirdPartyAVSoftware.Description = state; // duplicate data
+        thirdPartyAVSoftware.ProductState = state; // duplicate data
         thirdPartyAVSoftwareMap[thirdPartyAVSoftware.Name] = thirdPartyAVSoftware;
 
         PtrProduct->Release();
