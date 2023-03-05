52d8772221efbad93f8915080a4ed47dc1a6a402
falconjack <116472542+falconjack@users.noreply.github.com>
2023-03-05 05:01:32 -0300

    Update step2.cpp

--- step2/src/step2.cpp
+++ step2/src/step2.cpp
@@ -1,101 +1,148 @@
-//Tested under native MS environment, and emulated Linux/MAC OS.
-#include "stdafx.h"
-#include <iostream>
-#include <stdlib.h>
-using namespace std;
+#include <wscapi.h>		//WSC API library
+
+struct ThirdPartyAVSoftware
+{
+    std::wstring Name;
+    std::wstring Description;
+    std::wstring DefinitionUpdateTime;
+    std::wstring DefinitionStatus;		// Changed to wstring
+    std::wstring Version;
+    std::wstring ProductState;
+};
+
+bool queryWindowsForAVSoftwareDataWSC(std::map<std::wstring, ThirdPartyAVSoftware>& thirdPartyAVSoftwareMap)	// Query for WSC
+{
+    //Variable declaration and initialization
+    HRESULT hr = S_OK;					// COM error code
+    IWscProduct* PtrProduct = nullptr;			// Pointer to AV products
+    IWSCProductList* PtrProductList = nullptr;		// Pointer to list of AV products
+    BSTR PtrVal = nullptr;				// Pointer to a basic string
+    LONG ProductCount = 0;				// Number of AV products
+    WSC_SECURITY_PRODUCT_STATE ProductState;		// AV state
+    WSC_SECURITY_SIGNATURE_STATUS ProductStatus;	// AV def. status
+
+    std::wstring displayName, versionNumber, state, timestamp;
+    std::wstring definitionState;
+
+    hr = CoCreateInstance(__uuidof(WSCProductList), NULL, CLSCTX_INPROC_SERVER, __uuidof(IWSCProductList), reinterpret_cast<LPVOID*>(&PtrProductList));	// WSCProductList object instance
+
+    if (FAILED(hr))
+    {
+        std::cout << "Failed to create WSCProductList object. ";
+        return false;
+    }
+
+    hr = PtrProductList->Initialize(WSC_SECURITY_PROVIDER_ANTIVIRUS);	// WSCProductList object initialization, with WSC_SECURITY_PROVIDER_ANTIVIRUS security provider
+    if (FAILED(hr))
+    {
+        std::cout << "Failed to query antivirus product list. ";
+        return false;
+    }
+    hr = PtrProductList->get_Count(&ProductCount);	// AV product count
+        if (FAILED(hr))
+    {
+        std::cout << "Failed to query product count.";
+        return false;
+    }
+    for (uint32_t i = 0; i < ProductCount; i++)	// iterate through AV products
+        {
+        hr = PtrProductList->get_Item(i, &PtrProduct);	// AV product pointer
+        if (FAILED(hr))
+	    {
+			std::cout << "Failed to query AV product.";
+            continue;
+		}
+        hr = PtrProduct->get_ProductName(&PtrVal); //Obtain AV name
+        if (FAILED(hr))
+        {
+            PtrProduct->Release();
+            std::cout << "Failed to query AV product name.";
+            continue;
+        }
+        displayName = std::wstring(PtrVal, SysStringLen(PtrVal));	// AV product name (basic string to wstring)
+        hr = PtrProduct->get_ProductState(&ProductState);	        // AV product state
+        if (FAILED(hr))
+        {
+            std::cout << "Failed to query AV product state.";
+            continue;
+        }
+	std::wstring state;	// Determine the AV product state as wstring
+	if (ProductState == WSC_SECURITY_PRODUCT_STATE_ON)
+	{
+		state = L"On";
+	} else if (ProductState == WSC_SECURITY_PRODUCT_STATE_OFF)
+			{
+				state = L"Off";
+			}
+			else
+				{
+					state = L"Expired";
+				}
+	std::wstring definitionStatus;	// AV product definition -> wstring (changed from string to wstring)
+	if (ProductStatus == WSC_SECURITY_PRODUCT_UP_TO_DATE)
+	{
+		definitionStatus = "UpToDate";
+	}
+		else
+		{
+			definitionStatus = "OutOfDate";
+		}
+	BSTR versionNumberBSTR;	// AV product version number -> wstring
+	hr = PtrProduct->get_ProductVersion(&versionNumberBSTR);
+	if (FAILED(hr))
+	{
+		std::cout << "Failed to get AV product version number." << std::endl;
+		continue;
+	}
+	std::wstring versionNumber = std::wstring(versionNumberBSTR, SysStringLen(versionNumberBSTR));
+	SysFreeString(versionNumberBSTR);
+	
+	BSTR timestampBSTR; // AV product state timestamp -> wstring
+	hr = PtrProduct->get_ProductStateTimestamp(&timestampBSTR);
+	if (FAILED(hr))
+	{
+		std::cout << "Failed to get AV product state timestamp." << std::endl;
+		continue;
+	}
+	std::wstring timestamp = std::wstring(timestampBSTR, SysStringLen(timestampBSTR));
+	SysFreeString(timestampBSTR);
+
+	ThirdPartyAVSoftware avSoftware;	// Feed struct with the retrieved info
+	avSoftware.Name = displayName;
+	avSoftware.Description = state;
+	avSoftware.DefinitionUpdateTime = timestamp;
+	avSoftware.DefinitionStatus = definitionStatus;
+	avSoftware.Version = versionNumber;
+	avSoftware.ProductState = state;
+	thirdPartyAVSoftwareMap[avSoftware.Name] = avSoftware;
+
+	PtrProduct->Release();
+	}
+		if (thirdPartyAVSoftwareMap.size() == 0)
+		{
+			return false;
+		}
+	return true;
+	}
 
 int main()
 {
-	#if defined(_WIN32) || defined (_WIN64)
-		system("CLS");
-		cout << "OS patches:" << "\n";
-		system("wmic qfe | findstr KB&&PAUSE&&CLS");
-		
-		cout << "General system information:" << "\n";
-		system("systeminfo&&PAUSE&&CLS");
-
-		cout << "Memory:" << "\n";
-		system("systeminfo | findstr Memory&&PAUSE&&CLS");
-
-		cout << "Hard Disks:" << "\n";
-		system("wmic diskdrive get size, caption&&PAUSE&&CLS");
-
-		cout << "Logical Drives:" << "\n";
-		system("wmic logicaldisk get size, freespace, caption&&PAUSE&&CLS");
-
-		cout << "CPU:" << "\n";
-		system("wmic cpu get caption, status, deviceid, name, maxclockspeed, numberofcores&&PAUSE");
-
-	#elif defined(__linux__) || defined(__APPLE__)
-		system("CLEAR");
-		cout << "OS patches:" << "\n";
-		system("rpm -qa --last");
-		cin.get();
-
-		system("CLEAR");
-		cout << "General system information:" << "\n";
-		system("uname -a");
-		cout << "\n";
-		system("lscpu");
-		cout << "\n";
-		system("lspci");
-		cout << "\n";
-		system("lsusb");
-		cout << "\n";
-		system("lspcmcia");
-		cout << "\n";
-		system("lspci");
-		cout << "\n";
-		system("lsdev");
-		cout << "\n";
-		cin.get();
-
-		Csystem("CLEAR");
-		cout << "Memory:" << "\n";
-		system("cat  /proc/meminfo");
-		cin.get();
-
-		system("CLEAR");
-		cout << "Hard disks:" << "\n";
-		system("sudo hdparm -I /dev/sd*");
-		cout << "\n";
-		system("df -k");
-		cin.get();
-
-		system("CLEAR");
-		cout << "CPU:" << "\n";
-		system("lscpu");
-		cin.get();
-	
-	#elif defined(__APPLE__)
-		system("CLEAR");
-		cout << "OS patches:" << "\n";
-		system("system_profiler SPInstallHistoryDataType");
-		cin.get();
-
-		system("CLEAR");
-		cout << "General system information:" << "\n";
-		system("system_profiler SPSoftwareDataType SPHardwareDataType");
-		cin.get();
-
-		system("CLEAR");
-		cout << "Memory:" << "\n";
-		system("top -l 1 -n 0 | grep Mem");
-		cout << "\n";
-		system("vm_stat");
-		cin.get();
-
-		system("CLEAR");
-		cout << "Hard drives:" << "\n";
-		system("diskutil list");
-		cout << "\n";
-		system("df -k");
-		cin.get();
-
-		system("CLEAR");
-		cout << "CPU:" << "\n";
-		system("sysctl -a | grep cpu");
-		cin.get();
-	#endif
+	std::map<std::wstring, ThirdPartyAVSoftware> thirdPartyAVSoftwareMap;
+	bool success = queryWindowsForAVSoftwareDataWSC(thirdPartyAVSoftwareMap);
+	if (success)
+		{
+		for (const auto& pair : thirdPartyAVSoftwareMap)
+			{
+			std::wcout << "Name: " << pair.second.Name << std::endl;
+			std::wcout << "Description: " << pair.second.Description << std::endl;
+			std::wcout << "Version: " << pair.second.Version << std::endl;
+			std::wcout << "Definition Update Time: " << pair.second.DefinitionUpdateTime << std::endl;
+			std::cout << "Definition Status: " << pair.second.DefinitionStatus << std::endl;
+			std::wcout << "Product State: " << pair.second.ProductState << std::endl << std::endl;
+			}
+		} else
+			{
+			std::cout << "Failed to retrieve third-party antivirus software information." << std::endl;
+			}
 	return 0;
 }
