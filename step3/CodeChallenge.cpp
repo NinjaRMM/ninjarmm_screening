3d9d9d300e199de97b58f23e2efa38a2159b9e4f
Adriano Delatorre Kairalla <adriano.kairalla@kryptus.com>
2023-03-20 23:29:56 -0300

    chore: Code challenge completed

--- step3/CodeChallenge.cpp
+++ step3/CodeChallenge.cpp
@@ -1,9 +1,9 @@
 /*
 
 NINJARMM Code Challenge
- 
-Please review the below code. 
-We do expect you to execute this code. 
+
+Please review the below code.
+We do expect you to execute this code.
 
 This file contains the 'main' function. Program execution begins and ends there.
 */
@@ -12,41 +12,56 @@ This file contains the 'main' function. Program execution begins and ends there.
 
 typedef struct SampleObject1
 {
-    int getResult(const int& param1, const int& param2, const int& param3) const
+    int getResult(const int &param1, const int &param2, const int &param3) const
     {
         return (param1 + param2 + param3 > 0) ? 1 : 0;
     }
-}OBJ_INT;
+} OBJ_INT;
 
 typedef struct SampleObject2
 {
-    std::string getResult(const int& param1, const int& param2, const int& param3) const
+    std::string getResult(const int &param1, const int &param2, const int &param3) const
     {
         return (param1 + param2 + param3 > 0) ? "true" : "false";
     }
 
-}OBJ_STR;
+} OBJ_STR;
 
 OBJ_INT objInt;
 OBJ_STR objStr;
 
-
 //<<<<<<<< START OF EDITABLE SECTION OF CODE <<<<<<<<<<<
 
-//Here's a template function that can accept a sample object and 3 integer parameters. It checks if the sum of the integer params is positive or not.
-//The third integer param is optional so it's declared as a default param
-//Inside the function is some existing code that needs to exist, but not relevant to this exercise. 
+// Here's a template function that can accept a sample object and 3 integer parameters. It checks if the sum of the integer params is positive or not.
+// The third integer param is optional so it's declared as a default param
+// Inside the function is some existing code that needs to exist, but not relevant to this exercise.
 template <typename T, typename U>
-T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 = 0)
+T checkIfPositive(const U &u, const int &param1, const int &param2, int param3 = 0)
 {
 
-    // Assume there's already some existing code in here, some business logic that we are not interested in this exercise. 
+    // Assume there's already some existing code in here, some business logic that we are not interested in this exercise.
     /*
 
     SOME EXISTING CODE
 
     */
+    return u.getResult(param1, param2, param3);
+}
 
+// Because there is no way to differentiate between param 3 and total, the total will only be used when up to param 3 exists
+// Therefore we create a new template with param1, param2 and param3 as constant integers and total as a parameter integer without initialization
+//(Because it would conflict with the working of the other template, giving several errors).
+template <typename T, typename U>
+T checkIfPositive(const U &u, const int &param1, const int &param2, const int &param3, int &total)
+{
+
+    // Assume there's already some existing code in here, some business logic that we are not interested in this exercise.
+    /*
+
+    SOME EXISTING CODE
+
+    */
+    total = param1 + param2 + param3;
     return u.getResult(param1, param2, param3);
 }
 
@@ -56,7 +71,7 @@ int main()
 {
     //<<<<<<<< START OF NON-EDITABLE SECTION OF CODE <<<<<<<<<<<
 
-    //The following calls are already coded in a lot of places throughout the project so you can't change these
+    // The following calls are already coded in a lot of places throughout the project so you can't change these
     int iResult = checkIfPositive<bool, OBJ_INT>(objInt, 1, 2);
     std::cout << iResult << std::endl;
     iResult = checkIfPositive<bool, OBJ_INT>(objInt, 1, -2);
@@ -73,16 +88,15 @@ int main()
 
     //<<<<<<<< END OF NON-EDITABLE SECTION OF CODE <<<<<<<<<<<
 
-    //Now here's a new requirement where you need to get the total as an output param of checkIfPositive() template function
-    //You can't touch the previous calls to checkIfPositive because they are used in a lot of different places throughout the code base.
-    //How are you going to solve this problem? 
-    //You can only change the code inside the EDITABLE SECTION OF CODE
-    //Hint, the following calls should now be valid moving forward:
-    /*
+    // Now here's a new requirement where you need to get the total as an output param of checkIfPositive() template function
+    // You can't touch the previous calls to checkIfPositive because they are used in a lot of different places throughout the code base.
+    // How are you going to solve this problem?
+    // You can only change the code inside the EDITABLE SECTION OF CODE
+    // Hint, the following calls should now be valid moving forward:
     int total = 0;
 
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2);
-    std::cout << iResult <<  std::endl;
+    std::cout << iResult << std::endl;
 
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2, -3);
     std::cout << iResult << std::endl;
@@ -91,16 +105,13 @@ int main()
     std::cout << iResult << " " << total << std::endl;
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2);
-    std::cout << sResult <<  std::endl;
+    std::cout << sResult << std::endl;
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, -3);
-    std::cout << sResult <<  std::endl;
+    std::cout << sResult << std::endl;
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total); //<-- Total is output param
     std::cout << sResult << " " << total << std::endl;
-    */
 
     return 0;
-
 }
-
