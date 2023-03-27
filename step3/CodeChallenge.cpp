ef089bffc04ef822a0924c539fd7f54a8635fe8d
Stephen 'Thor' Pilcher <spilcher@knights.ucf.edu>
2023-03-27 01:33:58 -0400

    Implementation of code challenge

--- step3/CodeChallenge.cpp
+++ step3/CodeChallenge.cpp
@@ -32,6 +32,12 @@ OBJ_STR objStr;
 
 
 //<<<<<<<< START OF EDITABLE SECTION OF CODE <<<<<<<<<<<
+template <typename T, typename U>
+T checkIfPositive(const U& u, const int& param1, const int& param2, const int& param3, int& param4)
+{
+    param4 = param1 + param2 + param3;
+    return checkIfPositive<T, U>(u, param1, param2, param3);
+}
 
 //Here's a template function that can accept a sample object and 3 integer parameters. It checks if the sum of the integer params is positive or not.
 //The third integer param is optional so it's declared as a default param
@@ -78,7 +84,7 @@ int main()
     //How are you going to solve this problem? 
     //You can only change the code inside the EDITABLE SECTION OF CODE
     //Hint, the following calls should now be valid moving forward:
-    /*
+    
     int total = 0;
 
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2);
@@ -98,7 +104,7 @@ int main()
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total); //<-- Total is output param
     std::cout << sResult << " " << total << std::endl;
-    */
+    
 
     return 0;
 
