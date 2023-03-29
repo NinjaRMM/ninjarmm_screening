0dc132f313901a50dca5656fd1786d8fbb1df1d5
Henrique Awoyama Klein <henryaklein@gmail.com>
2023-03-29 18:21:21 -0300

    Feat: Solving the requirement by overloading

--- step3/CodeChallenge.cpp
+++ step3/CodeChallenge.cpp
@@ -16,7 +16,7 @@ typedef struct SampleObject1
     {
         return (param1 + param2 + param3 > 0) ? 1 : 0;
     }
-}OBJ_INT;
+} OBJ_INT;
 
 typedef struct SampleObject2
 {
@@ -25,22 +25,21 @@ typedef struct SampleObject2
         return (param1 + param2 + param3 > 0) ? "true" : "false";
     }
 
-}OBJ_STR;
+} OBJ_STR;
 
 OBJ_INT objInt;
 OBJ_STR objStr;
-
 
 //<<<<<<<< START OF EDITABLE SECTION OF CODE <<<<<<<<<<<
 
 //Here's a template function that can accept a sample object and 3 integer parameters. It checks if the sum of the integer params is positive or not.
 //The third integer param is optional so it's declared as a default param
-//Inside the function is some existing code that needs to exist, but not relevant to this exercise. 
+//Inside the function is some existing code that needs to exist, but not relevant to this exercise.
 template <typename T, typename U>
 T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 = 0)
 {
 
-    // Assume there's already some existing code in here, some business logic that we are not interested in this exercise. 
+    // Assume there's already some existing code in here, some business logic that we are not interested in this exercise.
     /*
 
     SOME EXISTING CODE
@@ -50,6 +49,15 @@ T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 =
     return u.getResult(param1, param2, param3);
 }
 
+// Using simple overloading
+template <typename T, typename U>
+T checkIfPositive(const U& u, const int& param1, const int& param2, int param3, int& param4)
+{
+
+    param4 = param1 + param2 + param3;
+
+    return checkIfPositive<T, U>(u, param1, param2, param3);
+}
 //<<<<<<<< END OF EDITABLE SECTION OF CODE <<<<<<<<<<<
 
 int main()
@@ -75,14 +83,14 @@ int main()
 
     //Now here's a new requirement where you need to get the total as an output param of checkIfPositive() template function
     //You can't touch the previous calls to checkIfPositive because they are used in a lot of different places throughout the code base.
-    //How are you going to solve this problem? 
+    //How are you going to solve this problem?
     //You can only change the code inside the EDITABLE SECTION OF CODE
     //Hint, the following calls should now be valid moving forward:
-    /*
+
     int total = 0;
 
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2);
-    std::cout << iResult <<  std::endl;
+    std::cout << iResult << std::endl;
 
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2, -3);
     std::cout << iResult << std::endl;
@@ -91,16 +99,13 @@ int main()
     std::cout << iResult << " " << total << std::endl;
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2);
-    std::cout << sResult <<  std::endl;
+    std::cout << sResult << std::endl;
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, -3);
-    std::cout << sResult <<  std::endl;
+    std::cout << sResult << std::endl;
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total); //<-- Total is output param
     std::cout << sResult << " " << total << std::endl;
-    */
 
     return 0;
-
 }
-
