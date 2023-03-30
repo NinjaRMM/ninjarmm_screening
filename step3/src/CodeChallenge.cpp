2bbbe1679a1b2fd1f1e0c055005e9bc521bceb27
JoMike Milly <jilesione@gmail.com>
2023-03-30 15:31:00 -0400

    Step 3 implementation

--- step3/src/CodeChallenge.cpp
+++ step3/src/CodeChallenge.cpp
@@ -32,10 +32,14 @@ OBJ_STR objStr;
 
 
 //<<<<<<<< START OF EDITABLE SECTION OF CODE <<<<<<<<<<<
+#include <cassert> //placing inside editable secition of code
 
 //Here's a template function that can accept a sample object and 3 integer parameters. It checks if the sum of the integer params is positive or not.
 //The third integer param is optional so it's declared as a default param
 //Inside the function is some existing code that needs to exist, but not relevant to this exercise. 
+
+//#define USE_SOLUTION_TWO
+#ifndef USE_SOLUTION_TWO
 template <typename T, typename U>
 T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 = 0)
 {
@@ -50,6 +54,34 @@ T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 =
     return u.getResult(param1, param2, param3);
 }
 
+//Simply add an overload for the new functionaliy
+template <typename T, typename U>
+T checkIfPositive(const U& u, const int& param1, const int& param2, int param3, int& total)
+{
+    total = param1 + param2 + param3;
+
+    //call the existing checkIfPositive under the condition we want to reuse the same business logic
+    return checkIfPositive<T, U>(u, param1, param2, param3);
+}
+#else //Solution 2
+//we create a dummy int variable because references must be initialized with a valid lvalue
+//This preserves the default nature of param3
+static int s_dummy_total = 0;
+template <typename T, typename U>
+T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 = 0, int& total = s_dummy_total)
+{
+
+    // Assume there's already some existing code in here, some business logic that we are not interested in this exercise. 
+    /*
+
+    SOME EXISTING CODE
+
+    */
+    total = param1 + param2 + param3;
+    return u.getResult(param1, param2, param3);
+}
+
+#endif
 //<<<<<<<< END OF EDITABLE SECTION OF CODE <<<<<<<<<<<
 
 int main()
@@ -78,9 +110,9 @@ int main()
     //How are you going to solve this problem? 
     //You can only change the code inside the EDITABLE SECTION OF CODE
     //Hint, the following calls should now be valid moving forward:
-    /*
+    
     int total = 0;
-
+    std::cout << std::endl;
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2);
     std::cout << iResult <<  std::endl;
 
@@ -98,8 +130,21 @@ int main()
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total); //<-- Total is output param
     std::cout << sResult << " " << total << std::endl;
-    */
 
+    //UNIT TESTS
+    //For time and brevity i won't create a unit test project for this file
+    sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total);
+    assert(6 == total);
+    assert(sResult == "true");
+
+    sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, -3, total);
+    assert(0 == total);
+    assert("false" == sResult);
+
+
+    iResult = checkIfPositive<int>(objInt, 1, 2, -3, total);
+    assert(0 == total);
+    assert(0 == iResult);
     return 0;
 
 }
