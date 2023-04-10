3cc759fb261f847dbd81f2372fed1650d1781b68
Jaime Andres Castillo Leon <jacastillol@gmail.com>
2023-04-10 14:34:08 -0500

    feat+test: Programmer class and test doWork

--- step1/the_jobs.cpp
+++ step1/the_jobs.cpp
@@ -1,5 +1,9 @@
 /*
   g++ -std=c++17 -I . -o the_jobs.out the_jobs.cpp
+
+  - To run the tests with info, run:
+
+  ./the_jobs.out -s
 */
 
 #include <string>
@@ -15,6 +19,9 @@ class Job
   int _hours_req{0};
 
  public:
+  Job() = default;
+  Job(const std::string& name, const std::string& description, int hours)
+   : _name{name}, _description{description}, _hours_req{hours} {}
 
   std::string get_name() const;
   std::string get_description() const;
@@ -32,12 +39,35 @@ TEST_CASE("create job")
   SUBCASE("default constructor")
   {
     // compile-time tested as:
-    static_assert(std::is_default_constructible<Job>::value == true);
     static_assert(std::is_nothrow_default_constructible<Job>::value == true);
+    static_assert(
+      std::is_constructible<Job,std::string,std::string,int>::value == true
+    );
     // run-time tested as:
     Job j;
-    REQUIRE(j.get_name() == "");
-    REQUIRE(j.get_description() == "");
-    REQUIRE(j.get_hours_required() == 0);
+    CHECK(j.get_name() == "");
+    CHECK(j.get_description() == "");
+    CHECK(j.get_hours_required() == 0);
+  }
+}
+
+class Programmer : public Job
+{
+ public:
+  Programmer() noexcept
+   : Job{"programmer", "creating solutions in the world", 40} {}
+};
+
+TEST_CASE("do work for a Programmer")
+{
+  SUBCASE("programer do work")
+  {
+    // compile-time tested as:
+    static_assert(std::is_default_constructible<Programmer>::value == true);
+    // run-time tested as:
+    Programmer p;
+    CHECK(p.get_name() == "programmer");
+    CHECK(p.get_description() == "creating solutions in the world");
+    CHECK(p.get_hours_required() == 40);
   }
 }
