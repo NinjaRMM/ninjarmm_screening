bb34d363b3fddc680287fabd56aaf1072ac05b7c
Eberty Alves <eberty.silva@hotmail.com>
2023-05-03 21:00:21 +0100

    Solve code challenge on Step 3

--- step3/CodeChallenge.cpp
+++ step3/CodeChallenge.cpp
@@ -50,6 +50,15 @@ T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 =
     return u.getResult(param1, param2, param3);
 }
 
+// Please see comments on line 92
+// I also have noticed that although param3 has a different argument type (non-constant copy), it's being used as a const int in the other functions.
+template <typename T, typename U>
+T checkIfPositive(const U& u, const int& param1, const int& param2, int param3, int &total)
+{
+    total = param1 + param2 + param3;
+    return checkIfPositive<T, U>(u, param1, param2, param3);
+}
+
 //<<<<<<<< END OF EDITABLE SECTION OF CODE <<<<<<<<<<<
 
 int main()
@@ -78,7 +87,55 @@ int main()
     //How are you going to solve this problem? 
     //You can only change the code inside the EDITABLE SECTION OF CODE
     //Hint, the following calls should now be valid moving forward:
+
     /*
+     Since the instructions explicitly state that we can only change the code inside the "EDITABLE SECTION OF CODE",
+     it's best to leave the code below untouched, even is 'not' part of NON-EDITABLE SECTION OF CODE (see line 83).
+
+     However, regarding the instructions, which state that we cannot touch the previous calls to checkIfPositive,
+     it's important to note that these instructions only apply to the previous calls in the provided code snippet (code base),
+     and not to future calls that we may need to add (lines 145 and 154).
+
+     Therefore, it would be acceptable to modify the calls to checkIfPositive that were presented in the code below,
+     as long as we don't modify the calls in other parts of the codebase.
+
+     With that said, it would be a good idea to modify the following calls:
+
+        iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2, 3, total);
+        sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total);
+    to
+        iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2, 3, &total);
+        sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, &total);
+
+     since using a pointer directly as output is more intuitive and common in C++.
+     This would avoid misunderstandings regarding the output param and other input variables.
+
+     We can improve the code by modifying the original checkIfPositive function in the editable section to include
+     the total output, so that we don't need to create a new function:
+
+    ```
+        template <typename T, typename U>
+        T checkIfPositive(const U &u, const int &param1, const int &param2, int param3 = 0, int *total = nullptr)
+        {
+            int sum = param1 + param2 + param3;
+
+            if (total != nullptr)
+            {
+                *total = sum;
+            }
+
+            // SOME EXISTING CODE
+
+            return u.getResult(param1, param2, param3);
+        }
+    ```
+
+     This is my best answer.
+
+     It's important to note that the modifications suggested above will not alter the behavior of the existing calls to checkIfPositive in the codebase.
+     These modifications only affect future calls that require the output parameter (lines 145 and 154).
+    */
+
     int total = 0;
 
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2);
@@ -98,7 +155,6 @@ int main()
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total); //<-- Total is output param
     std::cout << sResult << " " << total << std::endl;
-    */
 
     return 0;
 
