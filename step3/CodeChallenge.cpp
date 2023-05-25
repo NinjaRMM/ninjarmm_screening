9739f6d17543808fff529b7916c804cbf7337364
Ivo Machado <ivo.souza@tpv-tech.com>
2023-05-25 18:38:31 -0400

    Implement step3

--- step3/CodeChallenge.cpp
+++ step3/CodeChallenge.cpp
@@ -36,9 +36,14 @@ OBJ_STR objStr;
 //Here's a template function that can accept a sample object and 3 integer parameters. It checks if the sum of the integer params is positive or not.
 //The third integer param is optional so it's declared as a default param
 //Inside the function is some existing code that needs to exist, but not relevant to this exercise. 
-template <typename T, typename U>
-T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 = 0)
+template <typename T, typename U, typename ...TotalArg>
+T checkIfPositive(const U& u, const int& param1, const int& param2,
+                  int param3 = 0, TotalArg&...total)
 {
+    static_assert(sizeof...(total) <= 1, "too many args for total");
+    if constexpr (sizeof...(total) == 1) {
+        (total,...) = param1 + param2 + param3;
+    }
 
     // Assume there's already some existing code in here, some business logic that we are not interested in this exercise. 
     /*
@@ -78,7 +83,7 @@ int main()
     //How are you going to solve this problem? 
     //You can only change the code inside the EDITABLE SECTION OF CODE
     //Hint, the following calls should now be valid moving forward:
-    /*
+
     int total = 0;
 
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2);
@@ -98,7 +103,6 @@ int main()
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total); //<-- Total is output param
     std::cout << sResult << " " << total << std::endl;
-    */
 
     return 0;
 
