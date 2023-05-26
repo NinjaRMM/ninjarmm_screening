1a69c72eeafd8fa9d7cf5a630bd4c08872f628f1
Jesus Ramirez N <jesshack10@gmail.com>
2023-05-26 10:50:32 -0600

    Step1: created basic main file

---
+++ step1/src/jrn_step1.cpp
@@ -0,0 +1,10 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+int main() {
+
+    return 0;
+}
