70586311aa3c60e2a4bdd79d424a645015ea4212
Jesus Ramirez N <jesshack10@gmail.com>
2023-05-26 10:57:45 -0600

    Step1: Added Programmer subclass

--- step1/src/jrn_step1.cpp
+++ step1/src/jrn_step1.cpp
@@ -20,6 +20,26 @@ class Job {
     std::string m_description{"This is my Job"};
     int m_requiredHours{8};
 };
+
+//Programmer Job's child sub class
+class Programmer: public Job {
+    
+    public:
+    Programmer(){
+        m_name = "Programmer";
+        m_description = " doing my Programmer Job";
+        m_requiredHours = 8;
+    }
+    ~Programmer(){}
+    
+    std::string getName()  {return m_name;}
+    std::string getDescription()  {return m_description;}
+    int getRequiredHours()  {return m_requiredHours;}
+    void DoWork()  {
+        std::cout<<"My work involves" + m_description;
+    }
+
+};
 int main() {
 
     return 0;
