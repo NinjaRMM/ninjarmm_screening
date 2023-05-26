b53f21f3d3b1cf0187a7289a56fe10c1fbd92621
Jesus Ramirez N <jesshack10@gmail.com>
2023-05-26 11:17:14 -0600

    Step3: Extending checkIfPositive function
    -New parameter 'total' has been added, which will return
    the total sum of the elements in the templated function.
    -Commented test code has been uncommented to ensure overall function behavior

--- step3/CodeChallenge.cpp
+++ step3/CodeChallenge.cpp
@@ -37,7 +37,7 @@ OBJ_STR objStr;
 //The third integer param is optional so it's declared as a default param
 //Inside the function is some existing code that needs to exist, but not relevant to this exercise. 
 template <typename T, typename U>
-T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 = 0)
+T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 = 0, const int& total = 0)
 {
 
     // Assume there's already some existing code in here, some business logic that we are not interested in this exercise. 
@@ -46,7 +46,10 @@ T checkIfPositive(const U& u, const int& param1, const int& param2, int param3 =
     SOME EXISTING CODE
 
     */
-
+    const int* pTotal = &total;
+    int *vTotal = const_cast<int *>(pTotal);
+    *vTotal = param1 + param2 + param3;
+    
     return u.getResult(param1, param2, param3);
 }
 
@@ -78,7 +81,7 @@ int main()
     //How are you going to solve this problem? 
     //You can only change the code inside the EDITABLE SECTION OF CODE
     //Hint, the following calls should now be valid moving forward:
-    /*
+
     int total = 0;
 
     iResult = checkIfPositive<int, OBJ_INT>(objInt, 1, 2);
@@ -98,9 +101,7 @@ int main()
 
     sResult = checkIfPositive<std::string, OBJ_STR>(objStr, 1, 2, 3, total); //<-- Total is output param
     std::cout << sResult << " " << total << std::endl;
-    */
 
     return 0;
 
 }
-
