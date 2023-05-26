19cefc3fcb7817b0387fe31c26be41ee7eca6062
Jesus Ramirez N <jesshack10@gmail.com>
2023-05-26 10:51:13 -0600

    Step1: Added abstract Job class

--- step1/src/jrn_step1.cpp
+++ step1/src/jrn_step1.cpp
@@ -4,6 +4,22 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
+
+//Job Abstract Class
+class Job {
+    public:
+    Job(){}
+    virtual std::string getName() = 0;
+    virtual std::string getDescription() = 0;
+    virtual int getRequiredHours() = 0;
+    virtual void DoWork() = 0;
+    virtual ~Job(){};
+    
+    protected:
+    std::string m_name{"Job"};
+    std::string m_description{"This is my Job"};
+    int m_requiredHours{8};
+};
 int main() {
 
     return 0;
