9e935f078751eb5efcba990cef2e27be7c64689d
rmardones <rmardones@idirect.net>
2023-06-08 15:13:50 +0200

    Add cmake infrastructure for building

---
+++ step1/test/runtests.cpp
@@ -0,0 +1,9 @@
+/**
+ * \brief Entry point for unit tests.
+ */
+#include "gtest/gtest.h"
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
